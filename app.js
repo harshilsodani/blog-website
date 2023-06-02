c39683b8d0dc6ec98587edf37701560cd39b3cd6
harshilsodani <harshilsodani@users.noreply.github.com>
2023-06-02 13:35:38 +0530

    final

--- app.js
+++ app.js
@@ -1,67 +1,119 @@
-const express = require("express");
-const ejs = require("ejs");
-const _ = require("lodash");
+const express = require('express');
+const ejs = require('ejs');
+const mongoose = require('mongoose');
+const _ = require('lodash');
+const { Db } = require('mongodb');
+const PORT = process.env.PORT || 3000;
+const username = process.env.USERNAME;
+const password = process.env.PASSWORD;
 
 const homeStartingContent =
-  "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
-const aboutContent =
-  "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
-const contactContent =
-  "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+  'Welcome to DIGIVARTA, where technology meets inspiration! Dive into a world of innovation, knowledge, and endless possibilities. Explore captivating articles, expert analysis, and thought-provoking insights that unravel the mysteries of the tech universe. From the latest gadgets to groundbreaking advancements, we cover it all. Join our community of tech enthusiasts, entrepreneurs, and visionaries to embark on a journey of discovery. Discover the artistry behind coding, the marvels of AI, and the transformative power of digital disruption. Fuel your curiosity, ignite your passion, and unlock a world of digital wonders. Welcome to Digivarta - Where Tech Transforms and Inspires!';
+
+const aboutContent = [
+  "Welcome to Digivarta, an endeavor fueled by my passion for technology. I'm Harshil Sodani, a dedicated third-year student pursuing a Bachelor of Technology in Computer Science from SKIT Jaipur. As the sole member of this tech-driven journey, I aim to share my knowledge and insights with fellow tech enthusiasts like you.",
+  'Digivarta is a space where I explore the ever-evolving world of technology, unraveling its mysteries and showcasing its transformative power. Through engaging articles, informative tutorials, and curated resources, my goal is to bridge the gap between theory and practical application.',
+  "Join me on this exhilarating adventure as we dive into the realms of programming, artificial intelligence, and the latest tech trends. Together, let's embrace a future where technology knows no bounds.",
+  'Welcome to Digivarta, where technology meets inspiration!',
+];
+
+const contactContent = [
+  'We would love to hear from you! Whether you have questions, feedback, or simply want to connect, feel free to reach out using the contact information provided below. Your thoughts and inquiries are important to us, and we strive to respond as promptly as possible.',
+
+  'Email: [email protected]',
+  'Phone: +1-123-456-7890',
+
+  "Please don't hesitate to get in touch with us. We value your engagement and look forward to the opportunity to connect with fellow tech enthusiasts like yourself.",
+
+  "Stay inspired, stay curious, and let's continue exploring the ever-evolving world of technology together.",
+];
 
 const app = express();
 
 let posts = [];
 
-app.set("view engine", "ejs");
+app.set('view engine', 'ejs');
 
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static("public"));
+app.use(express.static('public'));
 
-app.get("/", function (req, res) {
-  res.render("home", {
-    startingContent: homeStartingContent,
-    posts: posts,
-  });
+mongoose.connect(
+  "mongodb+srv://" +
+    username +
+    ":" +
+    password +
+    "@cluster0.6dluecf.mongodb.net/blogDB",
+  { useNewUrlParser: true, useUnifiedTopology: true }
+);
+
+// mongoose.connect('mongodb://127.0.0.1:27017/blogDB', {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// });
+
+const postSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  content: { type: String, required: true },
 });
 
-app.get("/about", function (req, res) {
-  res.render("about", { aboutContent: aboutContent });
+const Post = mongoose.model('Post', postSchema);
+
+app.get('/', async function (req, res) {
+  try {
+    posts = [];
+    posts = await Post.find();
+
+    res.render('home', {
+      startingContent: homeStartingContent,
+      posts: posts,
+    });
+  } catch (err) {
+    console.log(err);
+  }
 });
 
-app.get("/contact", function (req, res) {
-  res.render("contact", { contactContent: contactContent });
+app.get('/about', function (req, res) {
+  res.render('about', { paragraphs: aboutContent });
 });
 
-app.get("/compose", function (req, res) {
-  res.render("compose");
+app.get('/contact', function (req, res) {
+  res.render('contact', { paragraphs: contactContent });
 });
 
-app.post("/compose", function (req, res) {
-  const post = {
+app.get('/compose', function (req, res) {
+  res.render('compose');
+});
+
+app.get('/posts/:postId', async function (req, res) {
+  const requestedPostId = req.params.postId;
+
+  try {
+    const post = await Post.findOne({ _id: requestedPostId });
+
+    res.render('post', {
+      title: post.title,
+      content: post.content,
+    });
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+app.post('/compose', async function (req, res) {
+  const post = new Post({
     title: req.body.postTitle,
     content: req.body.postBody,
-  };
-
-  posts.push(post);
-  res.redirect("/");
-});
-
-app.get("/posts/:postName", function (req, res) {
-  const requestedTitle = _.lowerCase(req.params.postName);
-
-  posts.forEach(function (post) {
-    const storedTitle = _.lowerCase(post.title);
-
-    if (storedTitle === requestedTitle) {
-      res.render("post", {
-        title: post.title,
-        content: post.content
-      });
-    }
   });
+
+  try {
+    await post.save();
+    res.redirect('/');
+    console.log('successfully saved');
+  } catch (err) {
+    console.log(err);
+  }
 });
 
-app.listen(3000, function () {
-  console.log("Server started on port 3000");
+app.listen(PORT, function () {
+  console.log(`server started at port ${PORT}`);
 });
