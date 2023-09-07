1479cf70efe541cb1d912ca6fba8cff88c1dbcb0
harshilsodani <harshilsodani@users.noreply.github.com>
2023-09-07 20:49:43 +0530

    db switch

--- app.js
+++ app.js
@@ -38,19 +38,19 @@ app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-// mongoose.connect(
-//   "mongodb+srv://" +
-//     DB_USERNAME +
-//     ":" +
-//     DB_PASSWORD +
-//     "@cluster0.6dluecf.mongodb.net/blogDB",
-//   { useNewUrlParser: true, useUnifiedTopology: true }
-// );
+mongoose.connect(
+  "mongodb+srv://" +
+    DB_USERNAME +
+    ":" +
+    DB_PASSWORD +
+    "@cluster0.6dluecf.mongodb.net/blogDB",
+  { useNewUrlParser: true, useUnifiedTopology: true }
+);
 
-mongoose.connect("mongodb://127.0.0.1:27017/blogDB", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+// mongoose.connect("mongodb://127.0.0.1:27017/blogDB", {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// });
 
 const postSchema = new mongoose.Schema({
   title: { type: String, required: true },
